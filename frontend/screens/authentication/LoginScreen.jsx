d1736641d0f0186347ced67802518af7725c12bb
Nisha-Bhattarai <c0840054@mylambton.ca>
2022-12-01 18:14:18 -0500

    Completed onboarding screen

--- frontend/screens/authentication/LoginScreen.jsx
+++ frontend/screens/authentication/LoginScreen.jsx
@@ -1,10 +1,11 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "twrnc";
 
 export default function LoginScreen() {
   return (
     <SafeAreaView>
-      <Text>LoginScreen</Text>
+      <Text style={tw`bg-red-500`}>LoginScreen</Text>
     </SafeAreaView>
   );
 }
