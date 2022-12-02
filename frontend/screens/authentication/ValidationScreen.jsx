e7f2a179486f53814aa81fc247b440f1a09d7604
Onurcan Sever <onsever@icloud.com>
2022-12-02 14:34:50 -0500

    changed overall implementation of storing image to the firebase storage

--- frontend/screens/authentication/ValidationScreen.jsx
+++ frontend/screens/authentication/ValidationScreen.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import {
-  View,
-  TouchableOpacity,
-  StyleSheet,
-  Image,
-  Text,
-  ActivityIndicator,
-} from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { View, TouchableOpacity, StyleSheet, Image } from "react-native";
 import { Camera, CameraType } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
-import { firebase } from "../../firebase/firebase";
-import { usePost } from "../../hooks/usePost";
+import { storage } from "../../firebase/firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import ProgressIndicator from "../../components/ProgressIndicator";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function ValidationScreen() {
+export default function ValidationScreen({ navigation }) {
   const [type, setType] = React.useState(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [hasPermission, setHasPermission] = React.useState(null);
   const [image, setImage] = React.useState(null);
   const [flash, setFlash] = React.useState(Camera.Constants.FlashMode.off);
   const [uploading, setUploading] = React.useState(false);
+  const [photoLoading, setPhotoLoading] = React.useState(false);
   const [resultText, setResultText] = React.useState("");
+  const [progress, setProgress] = React.useState(0);
 
   const camera = React.useRef(null);
-
-  const { post, loading, result, error, loaded } = usePost();
 
   React.useEffect(() => {
     (async () => {
@@ -34,22 +28,28 @@ export default function ValidationScreen() {
     })();
   }, []);
 
-  React.useEffect(() => {
-    if (loaded) {
-      if (error) {
-        console.log(error);
-      }
-      if (result) {
-        setResultText(result.data);
-      }
-    }
-  }, [loaded]);
+  const convertToBlob = (uri) => {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function () {
+        reject(new TypeError("Network request failed"));
+      };
+      xhr.responseType = "blob";
+      xhr.open("GET", uri, true);
+      xhr.send(null);
+    });
+  };
 
   const takePicture = async () => {
     if (camera) {
       try {
+        setPhotoLoading(true);
         const photo = await camera.current.takePictureAsync();
         setImage(photo.uri);
+        setPhotoLoading(false);
       } catch (err) {
         console.log("Error", err);
       }
@@ -57,87 +57,101 @@ export default function ValidationScreen() {
   };
 
   const uploadImage = async () => {
-    if (image === null) return;
-
-    const blob = await new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest();
-      xhr.onload = function () {
-        resolve(xhr.response);
-      };
-      xhr.onerror = function (e) {
-        console.log(e);
-        reject(new TypeError("Network request failed"));
-      };
-      xhr.responseType = "blob";
-      xhr.open("GET", image, true);
-      xhr.send(null);
-    });
-
-    const filename = image.substring(image.lastIndexOf("/") + 1);
-    const ref = firebase.storage().ref().child(filename);
-    const snapshot = await ref.put(blob);
-
-    snapshot.on(
-      firebase.storage.TaskEvent.STATE_CHANGED,
-      () => {
-        setUploading(true);
-      },
-      (error) => {
-        setUploading(false);
-        console.log(error);
-        blob.close();
-        return;
-      },
-      () => {
-        snapshot.snapshot.ref.getDownloadURL().then((url) => {
-          setUploading(false);
-          console.log("Download URL: ", url);
-          setImage(url);
-          blob.close();
-          return url;
-        });
-      }
-    );
-  };
-
-  const extractTextFromImage = async () => {
     try {
-      await post("auth/id-scan", { url: image });
-      console.log(result);
+      setUploading(true);
+      const blob = await convertToBlob(image);
+      const filename = image.substring(image.lastIndexOf("/") + 1);
+      const storageRef = ref(storage, `images/${filename}`);
+      const uploadTask = uploadBytesResumable(storageRef, blob, {
+        contentType: "image/jpeg",
+      });
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(progress);
+          console.log("Upload is " + progress + "% done");
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          switch (error.code) {
+            case "storage/unauthorized":
+              console.log("User doesn't have permission to access the object");
+              break;
+            case "storage/canceled":
+              console.log("User canceled the upload");
+              break;
+            case "storage/unknown":
+              break;
+          }
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            setUploading(false);
+            navigation.navigate("Verification", { downloadURL });
+          });
+        }
+      );
     } catch (err) {
-      console.log(err);
+      console.log("Error", err);
+      setUploading(false);
     }
   };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      {!image ? (
-        <Camera
-          ref={camera}
-          style={styles.camera}
-          type={type}
-          flashMode={flash}
-        >
-          <View style={styles.cameraActionsView}>
-            <TouchableOpacity
-              style={styles.captureButton}
-              onPress={takePicture}
-            />
-          </View>
-        </Camera>
+    <>
+      {uploading ? (
+        <ProgressIndicator progress={progress} />
       ) : (
-        <Image source={{ uri: image }} style={styles.camera} />
-      )}
-      {image ? (
-        <View>
-          <Text onPress={() => setImage(null)}>TAKE PICTURE AGAIN</Text>
-          <Text onPress={uploadImage}>Save</Text>
-          <Text onPress={() => extractTextFromImage()}>Extract Text</Text>
+        <View style={{ flex: 1 }}>
+          {!image && !uploading ? (
+            <Camera
+              ref={camera}
+              style={styles.camera}
+              type={type}
+              flashMode={flash}
+            >
+              <View style={styles.cameraActionsView}>
+                <TouchableOpacity
+                  style={styles.captureButton}
+                  onPress={takePicture}
+                >
+                  <Ionicons name="camera" size={30} color="black" />
+                </TouchableOpacity>
+              </View>
+            </Camera>
+          ) : (
+            <Image source={{ uri: image }} style={styles.camera} />
+          )}
+          {image ? (
+            <View style={styles.afterCapturedOptionsView}>
+              <TouchableOpacity
+                style={styles.afterCaptureOption}
+                onPress={() => setImage(null)}
+              >
+                <Ionicons name="camera" size={30} color="black" />
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.afterCaptureOption}
+                onPress={uploadImage}
+              >
+                <Ionicons name="cloud-upload" size={30} color="black" />
+              </TouchableOpacity>
+            </View>
+          ) : (
+            <></>
+          )}
         </View>
-      ) : (
-        <></>
       )}
-    </SafeAreaView>
+    </>
   );
 }
 
@@ -149,7 +163,7 @@ const styles = StyleSheet.create({
   },
   cameraActionsView: {
     position: "absolute",
-    bottom: 0,
+    bottom: 20,
     left: 0,
     right: 0,
     backgroundColor: "transparent",
@@ -158,8 +172,29 @@ const styles = StyleSheet.create({
     alignItems: "center",
     padding: 10,
   },
+  afterCapturedOptionsView: {
+    position: "absolute",
+    bottom: 20,
+    left: 0,
+    right: 0,
+    backgroundColor: "transparent",
+    opacity: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 30,
+  },
+  afterCaptureOption: {
+    backgroundColor: "white",
+    borderRadius: "50%",
+    padding: 10,
+    borderWidth: 3,
+    borderColor: "black",
+  },
   captureButton: {
     alignItems: "center",
+    justifyContent: "center",
     width: 70,
     height: 70,
     backgroundColor: "white",
