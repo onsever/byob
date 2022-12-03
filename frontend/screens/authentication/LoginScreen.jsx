48bd82feb0b5c28537d49da47ae7393ca0f5ea16
Vishal <vishalk0220@gmail.com>
2022-12-03 12:07:02 -0500

    conflict resolved

--- frontend/screens/authentication/LoginScreen.jsx
+++ frontend/screens/authentication/LoginScreen.jsx
@@ -30,48 +30,7 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <SafeAreaView style={tw`flex-1`}>
-<<<<<<< HEAD
-      <Image
-        source={require("../../assets/loginBackground.png")}
-        style={tw`w-full h-80`}
-      />
-      <View style={tw`pr-10 pl-10 mt-5`}>
-        <View style={tw`items-center mb-5`}>
-          <Text style={tw`text-10 mb-2 text-bold`}>Wine and Dine!</Text>
-          <Text>Life's A Party! Are you ready?</Text>
-        </View>
-
-        <View style={tw`flex flex-row justify-between mb-5`}></View>
-        <View style={tw`flex flex-column`}>
-          <Input
-            inputStyles={"h-11 text-center mb-5"}
-            placeholder={`Enter Your Email`}
-            onAction={(text) => console.log(text)}
-          />
-          <Input
-            inputStyles={"h-11 text-center"}
-            placeholder={`Enter Your Password`}
-            secure={true}
-            onAction={(text) => console.log(text)}
-          />
-
-          <TouchableOpacity
-            style={tw`flex flex-row justify-center align-center`}
-          >
-            <Image source={require("../../assets/wine_glass.png")} />
-            <Text style={tw`text-[#640100] mt-12 ml-[-5%]`}>SIGN IN</Text>
-          </TouchableOpacity>
-        </View>
-        <View style={tw`flex flex-row justify-evenly items-center mb-5`}>
-          <View style={tw`border-b w-[35%] border-[#C5C5C5]`}></View>
-          <View style={tw`w-[30%]`}>
-            <Text style={tw`text-center text-[#C5C5C5]`}>OR</Text>
-          </View>
-          <View style={tw`border-b w-[35%] border-[#C5C5C5]`}></View>
-        </View>
-=======
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
->>>>>>> 5409432 (conflict resolved)
         <View>
           <Image
             source={require("../../assets/loginBackground.png")}
@@ -136,11 +95,7 @@ export default function LoginScreen({ navigation }) {
             </View>
           </View>
         </View>
-<<<<<<< HEAD
-      </View>
-=======
       </TouchableWithoutFeedback>
->>>>>>> dfc4fb2 (conflict resolved)
     </SafeAreaView>
   );
 }
