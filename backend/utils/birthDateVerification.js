4eccc656b895f86427e0b864b04406dd3778ec7e
Onurcan Sever <onsever@icloud.com>
2022-12-02 22:31:09 -0500

    birth date verification through id

---
+++ backend/utils/birthDateVerification.js
@@ -0,0 +1,66 @@
+const filterBySlash = (line) => {
+  const results = [];
+  const lines = line.split("\n");
+  let counter = 0;
+  lines.forEach((line) => {
+    if (line.includes("/")) {
+      counter++;
+      if (counter > 2) {
+        results.push(line);
+      }
+    }
+  });
+
+  return results;
+};
+
+const checkIfDate = (arr) => {
+  const results = [];
+  arr.forEach((line) => {
+    const split = line.split("/");
+    if (split.length === 3) {
+      results.push(line);
+    }
+  });
+
+  return results;
+};
+
+const findBirthDate = (data) => {
+  if (!data) {
+    console.log("Error: No data found.");
+    return null;
+  }
+
+  const filteredArr = filterBySlash(data);
+
+  if (filteredArr.length === 0) {
+    console.log("Error: No data found.");
+    return null;
+  }
+
+  const dateArr = checkIfDate(filteredArr);
+
+  if (dateArr.length === 0) {
+    console.log("Error: No data found.");
+    return null;
+  }
+
+  return dateArr.sort((a, b) => {
+    return Date.parse(a) - Date.parse(b);
+  })[0];
+};
+
+const verifyBirthDate = (data) => {
+  const currentDate = new Date();
+  const birthDate = new Date(findBirthDate(data));
+  const age = currentDate.getFullYear() - birthDate.getFullYear();
+  const m = currentDate.getMonth() - birthDate.getMonth();
+  if (m < 0 || (m === 0 && currentDate.getDate() < birthDate.getDate())) {
+    age--;
+  }
+
+  return age >= 18;
+};
+
+export { verifyBirthDate };
