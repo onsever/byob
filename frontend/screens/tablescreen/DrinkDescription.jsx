4c0558b43139e4f822d9e0bcb0f25f618620e9ab
Roch Bajracharya <rochbajracharya@Rochs-MacBook-Pro.local>
2022-12-10 18:02:06 -0500

    dynamic drink menu

--- frontend/screens/tablescreen/DrinkDescription.jsx
+++ frontend/screens/tablescreen/DrinkDescription.jsx
@@ -9,10 +9,13 @@ import {
 import React, { useState } from "react";
 import tw from "twrnc";
 import { Picker } from "@react-native-picker/picker";
+import { getDrinkImage } from "../../utils/DrinkData";
 
 const DrinkDescription = ({ navigation, route }) => {
   const { item, title } = route.params;
   const [selectedQty, setSelectedQty] = useState();
+  console.log("item.image", item.image);
+  const [base64Image, setBase64Image] = useState(item.image);
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -24,7 +27,21 @@ const DrinkDescription = ({ navigation, route }) => {
     <SafeAreaView style={tw`w-full h-full`}>
       <View style={tw`mx-10 my-5 flex`}>
         <View style={tw`flex items-center`}>
-          <Image source={item.image} style={tw`w-40 h-50 mb-5`} />
+          <Image
+            source={getDrinkImage(item.image)}
+            style={tw`w-40 h-50 mb-5`}
+          />
+          {/* <Image
+            style={{
+              width: 51,
+              height: 51,
+              resizeMode: "contain",
+            }}
+            source={{
+              uri: "data:image/png;base64," + base64Image,
+            }}
+          /> */}
+
           <Text style={tw`text-6 font-bold mb-2`}>{item.name}</Text>
         </View>
 
