d3fdb560f1c410b9d9252452c7be0ff5e4336026
Onurcan Sever <onsever@icloud.com>
2022-12-13 09:49:56 -0500

    completed order history and profile

--- frontend/screens/profile/AccountOrderHistoryScreen.jsx
+++ frontend/screens/profile/AccountOrderHistoryScreen.jsx
@@ -1,9 +1,24 @@
 import React from "react";
-import { Alert, View, ActivityIndicator } from "react-native";
+import {
+  Alert,
+  View,
+  ActivityIndicator,
+  SectionList,
+  Text,
+  Image,
+} from "react-native";
 import { useFetch } from "../../hooks/useFetch";
+import tw from "twrnc";
+import moment from "moment";
 
 export default function AccountOrderHistoryScreen({ navigation }) {
+  const [data, setData] = React.useState(null);
+  const [tableData, setTableData] = React.useState(null);
   const { loading, result, error, loaded, fetch } = useFetch();
+
+  const dateParser = (date) => {
+    return moment(date).format("MMMM Do YYYY");
+  };
 
   React.useLayoutEffect(() => {
     navigation.setOptions({
@@ -19,14 +34,72 @@ export default function AccountOrderHistoryScreen({ navigation }) {
     if (loaded) {
       if (error) {
         Alert.alert("Error", error);
+        setIsDataReady(false);
       }
       if (result) {
-        console.log(result);
+        setData(result);
       }
     }
   }, [loaded]);
 
+  React.useEffect(() => {
+    setTableData(
+      data
+        ?.map((item) => {
+          return {
+            title: dateParser(item?.createdAt),
+            data: item?.order?.concat(item?.drinkOrder),
+          };
+        })
+        .filter((i) => i?.data !== undefined)
+    );
+  }, [data]);
+
   return (
-    <View>{loading && <ActivityIndicator size="large" color="black" />}</View>
+    <View style={tw`w-full h-full items-center justify-start`}>
+      {loading && !loaded ? (
+        <ActivityIndicator
+          size="large"
+          color="black"
+          style={tw`items-center justify-center`}
+        />
+      ) : (
+        <View style={tw`w-full`}>
+          <SectionList
+            sections={tableData || []}
+            keyExtractor={(item, index) => item + index}
+            renderItem={({ item }) => {
+              console.log(item);
+              return (
+                <View
+                  style={tw`flex-row items-center px-4 mb-4 bg-white shadow-md py-2`}
+                >
+                  <Image
+                    source={{
+                      uri:
+                        item?.image ||
+                        "https://i.ytimg.com/vi/IVM_CQvgxCg/maxresdefault.jpg",
+                    }}
+                    style={tw`w-16 h-16 rounded-full`}
+                  />
+                  <View>
+                    <Text style={tw`font-semibold ml-4 mb-1`}>
+                      {item?.name}
+                    </Text>
+                    <Text style={tw`ml-4`}>Quantity: {item?.quantity}x</Text>
+                    <Text style={tw`ml-4`}>Price: {item?.price}$</Text>
+                  </View>
+                </View>
+              );
+            }}
+            renderSectionHeader={({ section: { title } }) => (
+              <View style={tw`bg-[#640100] px-4 py-2 mb-2`}>
+                <Text style={tw`font-semibold text-white`}>{title}</Text>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    </View>
   );
 }
--- frontend/screens/profile/ProfileScreen.jsx
+++ frontend/screens/profile/ProfileScreen.jsx
@@ -11,7 +11,6 @@ import { useDispatch } from "react-redux";
 import tw from "twrnc";
 import { logout } from "../../redux/features/authSlice";
 import { getData } from "../../utils/asyncStorage";
-import moment from "moment";
 import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function ProfileScreen({ navigation }) {
@@ -47,7 +46,7 @@ export default function ProfileScreen({ navigation }) {
         style={tw`flex flex-row bg-white px-10 py-7 justify-between mx-6 mb-2 rounded-lg shadow-md w-full`}
       >
         <TouchableOpacity
-          style={tw`flex flex-column items-center`}
+          style={tw`flex flex-col items-center`}
           onPress={() => {
             navigation.navigate("AccountDetailsScreen", {
               user: user,
@@ -75,21 +74,8 @@ export default function ProfileScreen({ navigation }) {
         </TouchableOpacity>
       </View>
 
-      {user ? (
-        <View>
-          <Text>
-            Name: {user.firstName} {user.lastName}
-          </Text>
-          <Text>Email: {user.email}</Text>
-          <Text>Phone: {user.phone}</Text>
-          <Text>DOB: {moment(user.dob).format("DD MMMM, YYYY")}</Text>
-        </View>
-      ) : (
-        <ActivityIndicator />
-      )}
-
       <TouchableOpacity
-        style={tw` bg-[#640100] rounded-full p-4 absolute bottom-3 right-3`}
+        style={tw` bg-[#640100] rounded-full p-4 absolute bottom-3 right-3 items-center justify-center`}
         onPress={() => {
           dispatch(logout());
           navigation.replace("Auth");
